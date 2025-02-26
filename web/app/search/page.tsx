481764a8616a2f3914dcd022a02c6a6c0bd7c639
scottroot <allanscotthendrix@gmail.com>
2025-02-26 02:59:44 -0500

    update search to optionally do basic full-text match

--- web/app/search/page.tsx
+++ web/app/search/page.tsx
@@ -1,14 +1,18 @@
 import ProductGrid from "@/components/ProductGrid";
-import {vectorSearch} from "@/lib/neo4j/queries/vectorSearch";
-import type {SearchParams} from "@/types";
+import { vectorSearch } from "@/lib/neo4j/queries/vectorSearch";
+import { textSearch } from "@/lib/neo4j/queries/textSearch";
+import type { SearchParams } from "@/types";
 
 
 export default async function SearchPage({ searchParams, }: { searchParams: SearchParams }) {
-  const query = (await searchParams).query as string|undefined;
+  const sp = await searchParams;
+  const query = sp.query as string|undefined;
+  const doVector = sp.vector === "true";
+  const limit = Number(sp.limit || 10);
 
   let results: null | any[] = null;
   if(query) {
-    results = await vectorSearch(query);
+    results = doVector ? await vectorSearch(query, limit) : await textSearch(query, limit);
   }
   return (
     <section className="container mx-auto max-w-7xl px-8 pb-20 gap-16 sm:p-20 sm:pt-4">
